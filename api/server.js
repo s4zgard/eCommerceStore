d204af86fa57e7fac142bc2f0d75aa8dc1114a65
s4zgard <s4zgard@gmail.com>
2024-05-30 15:00:20 +0500

    Mongoose and Models Setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
+connectDB();
 const port = process.env.PORT || 3000;
 
 const app = express();
